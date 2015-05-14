0ec1b3d8c3cf9ec5ec12a07ca9266aa82675cc31
Alex Clemons <painfulac@gmail.com>
2015-05-14 17:51:09 -0500

    Added some sweet ass particle effects
    See title. Planning on changing GUI to be on a wall so it's less
    disconcerting

---
+++ TimeWarp/Assets/TimeModify.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeModify : MonoBehaviour {
+
+	public GameObject thePlayer;
+
+	// Use this for initialization
+	void Start() 
+	{
+		thePlayer = GameObject.Find ("OVRPlayerController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float L = thePlayer.GetComponent<TimeScale> ().getTimeScale();
+		gameObject.particleSystem.playbackSpeed = L;
+	}
+}
