939ce00091b4e174f0bac8e531651130d9d596ff
Alex Clemons <painfulac@gmail.com>
2015-05-14 15:45:47 -0500

    Kevin is awesome

---
+++ TimeWarp/Assets/Update_Fade.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Update_Fade : MonoBehaviour {
+
+	public GameObject thePlayer;
+	public Image the_image;
+	private Color c;
+	private float L;
+	// Use this for initialization
+	void Start () {
+		thePlayer = GameObject.Find ("OVRPlayerController");
+		c = the_image.color;
+		L = thePlayer.GetComponent<TimeScale>().getTimeScale();
+		c.a = 0.0f;
+		the_image.color = c;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		L = thePlayer.GetComponent<TimeScale>().getTimeScale();
+		if (L < 1.0f) 
+		{
+			c.a = 0.1f *(-Mathf.Log (L));
+			the_image.color = c;
+		}
+		else 
+		{
+			c.a = 0.0f;
+			the_image.color = c;
+		}
+	}
+}
